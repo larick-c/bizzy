33a9a0ee6242dcce2103265b096a38f9da0b454b
Jess LoPiccolo <jesslopiccolo@Jesss-MacBook-Air.local>
2024-04-11 12:29:12 -0400

    amplify init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "bizzy5f02e94d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
